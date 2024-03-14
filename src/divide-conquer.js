b58f3fc742bc645bcd4daa4e6918fc183896c981
Christian Justin <christian.justin23@gmail.com>
2024-03-15 00:01:46 +0700

    delete and refactor : divide and conquer
    added divide and conquer function for more than 3 points

--- src/divide-conquer.js
+++ src/divide-conquer.js
@@ -1,6 +1,7 @@
 let bezierPoints = [];
 
-function createDivideConquerBezier(control1, control2, control3, banyakiterasi) {
+// Fungsi untuk yang 3 titik doang
+function divideConquerBezier3Points(control1, control2, control3, banyakiterasi) {
     bezierPoints = [];
     bezierPoints.push(control1); 
     isiTitikCurve(control1, control2, control3, 0, banyakiterasi);
@@ -27,9 +28,38 @@ function cariTitikTengah(controlPoint1, controlPoint2) {
     };
 }
 
-let control1 = {x: 0, y: 0};
-let control2 = {x: 50, y: 100};
-let control3 = {x: 100, y: 0};
-let banyakiterasi = 5;
-let points = createDivideConquerBezier(control1, control2, control3, banyakiterasi);
-console.log(points);
+// Fungsi yang buat cari bezier yang titiknya lebih dari 3
+function divideConquerBezierMultiple(points, t) {
+    if (points.length === 1) {
+        return points[0];
+    }
+
+    let newPoints = [];
+    for (let i = 0; i < points.length - 1; i++) {
+        let x = (1 - t) * points[i].x + t * points[i + 1].x;
+        let y = (1 - t) * points[i].y + t * points[i + 1].y;
+        newPoints.push({x: x, y: y});
+    }
+
+    return divideConquerBezierMultiple(newPoints, t);
+}
+
+// Fungsi yang buat cari bezier yang titiknya lebih dari 3 tapi pake built in function (Kurang bagus sih jujur)
+/*
+function multicurve(points, context) {
+    var p0, p1, midx, midy;
+
+    context.moveTo(points[0].x, points[0].y);
+
+    for(var i = 1; i < points.length - 2; i += 1) {
+        p0 = points[i];
+        p1 = points[i + 1];
+        midx = (p0.x + p1.x) / 2;
+        midy = (p0.y + p1.y) / 2;
+        context.quadraticCurveTo(p0.x, p0.y, midx, midy);
+    }
+    p0 = points[points.length - 2];
+    p1 = points[points.length - 1];
+    context.quadraticCurveTo(p0.x, p0.y, p1.x, p1.y);
+}
+*/
